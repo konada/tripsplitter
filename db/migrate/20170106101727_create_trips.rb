ae5b12d9ef1e4f963baf8060483b9432d744d546
konada <boards.of.konada@gmail.com>
2017-01-06 13:59:22 +0100

    Add Trip model

---
+++ db/migrate/20170106101727_create_trips.rb
@@ -0,0 +1,15 @@
+class CreateTrips < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trips do |t|
+      t.integer :people_count, default: 1, null: false
+      t.boolean :driver_included, default: true, null: false
+      t.integer :trip_distance
+      t.decimal :fuel_cost
+      t.decimal :avg_consumption
+      t.decimal :tanking_cost
+      t.decimal :cost_per_person
+
+      t.timestamps
+    end
+  end
+end
