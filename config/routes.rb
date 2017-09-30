555239757ae4da9c99f2316950fb368294c6db14
konada <boards.of.konada@gmail.com>
2017-09-30 13:45:53 +0200

    Add omniauth model changes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :trips, only: [:new, :index, :create]
   root 'trips#new'
 end
